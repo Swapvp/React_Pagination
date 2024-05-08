89dfeae7bec1e173973dca216b707db69959c43d
Swapvp <swapneshpawar1994@gmail.com>
2024-05-08 11:33:37 +0530

    pagination

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import PaginationTest from "./Components/PaginationTest";
+
+function App() {
+  return (
+    <>
+      <PaginationTest />
+    </>
+  );
+}
+
+export default App;
